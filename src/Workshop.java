e22dd854c7a2a8245e8c737dfb9328f5d4c4e635
AdelAkbi <mohamedadel.akbi.algerie@viacesi.fr>
2017-06-13 16:17:11 +0000

    first commit

---
+++ src/Workshop.java
@@ -0,0 +1,13 @@
+public class Workshop {
+/**
+* @param args
+*/
+
+public static void main(String[] args) {
+Personne unePersonne = new Personne();
+unePersonne.nom = "Diet";
+unePersonne.prenom = "Jean-Aymeric";
+unePersonne.direBonjour();
+
+}
+}
